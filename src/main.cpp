1619b940c5178a91ac1999bf53f84c30e2ae2411
Erwan Brunelliere <erwan.brunelliere@etudiant.univ-lr.fr>
2022-01-01 23:53:18 +0100

    zoom

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+#define WIDTH 1200
+#define HEIGHT 800
+
 int borned(double aX, double aY)
 {
     double x = 0, y = 0;
@@ -22,42 +25,57 @@ int borned(double aX, double aY)
     return 0;
 }
 
+void drawMandelbrot(double xMin, double xMax, double yMin, double yMax, double coefX, double coefY, SDL_Renderer * renderer)
+{
+    for(int i=0; i<WIDTH; ++i)
+        for(int j=0; j<HEIGHT; ++j)
+        {
+            double x = (i / coefX) + xMin;
+            double y = (j / coefY) + yMin;
+            int pow = borned(x, y);
+            int red = (pow*255) / 3;
+            if(pow > 0)
+                SDL_SetRenderDrawColor(renderer, 0, (3*pow)%256, 0, 255);
+            else
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderDrawPoint(renderer, i, j);
+        }
+
+    SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char** argv)
 {
-    int width = 1200;
-    int height = 800;
+    double xMin = -3.2, xMax = -0.2, yMin = -1, yMax = 1;
 
-    double coef = double(width) / 3;
+    double coefX = double(WIDTH) / (xMax - xMin);
+    double coefY = double(HEIGHT) / (yMax - yMin);
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("SortGUI",
                                 SDL_WINDOWPOS_UNDEFINED,
                                 SDL_WINDOWPOS_UNDEFINED,
-                                width,
-                                height,
+                                WIDTH,
+                                HEIGHT,
                                 SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    for(int i=0; i<width; ++i)
-        for(int j=0; j<height; ++j)
-        {
-            double x = (i / coef) - 2;
-            double y = (j / coef) - 1;
-            int pow = borned(x, y);
-            int red = (pow*255) / 3;
-            if(pow > 0)
-                SDL_SetRenderDrawColor(renderer, (3*pow)%256, (1*pow)%256, (10*pow)%256, 255);
-            else
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            SDL_RenderDrawPoint(renderer, i, j);
-        }
+    double xStep = 0.015;
+    double yStep = 0.01;
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
-    SDL_RenderPresent(renderer);
+    for(int i=0; i<100; ++i)
+    {
+        drawMandelbrot(xMin, xMax, yMin, yMax, coefX, coefY, renderer);
+        xMin += xStep;
+        xMax -= xStep;
+        yMin += yStep;
+        yMax -= yStep;
+        coefX = double(WIDTH) / (xMax - xMin);
+        coefY = double(HEIGHT) / (yMax - yMin);
+    }
 
     system("pause");
     return 0;
