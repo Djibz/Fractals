5424e37a3328d48c3578c19182e5284581145bae
Erwan Brunelliere <erwan.brunelliere@etudiant.univ-lr.fr>
2022-01-02 19:00:12 +0100

    long double

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -51,10 +51,46 @@ bool saveScreenshotBMP(string filepath, int nb, SDL_Window *window, SDL_Renderer
     return true;
 }
 
-int borned(double aX, double aY)
+void HSVcolor(float H, SDL_Renderer *renderer){
+    float S = 100, V = 100;
+    if(H>360 || H<0 || S>100 || S<0 || V>100 || V<0){
+        cout<<"The givem HSV values are not in valid range"<<endl;
+        return;
+    }
+    float s = S/100;
+    float v = V/100;
+    float C = s*v;
+    float X = C*(1-abs(fmod(H/60.0, 2)-1));
+    float m = v-C;
+    float r,g,b;
+    if(H >= 0 && H < 60){
+        r = C,g = X,b = 0;
+    }
+    else if(H >= 60 && H < 120){
+        r = X,g = C,b = 0;
+    }
+    else if(H >= 120 && H < 180){
+        r = 0,g = C,b = X;
+    }
+    else if(H >= 180 && H < 240){
+        r = 0,g = X,b = C;
+    }
+    else if(H >= 240 && H < 300){
+        r = X,g = 0,b = C;
+    }
+    else{
+        r = C,g = 0,b = X;
+    }
+    int R = (r+m)*255;
+    int G = (g+m)*255;
+    int B = (b+m)*255;
+    SDL_SetRenderDrawColor(renderer, R, G, B, 255);
+}
+
+int borned(long double aX, long double aY)
 {
-    double x = 0, y = 0;
-    for(int i=0; i < PRECISION; ++i)
+    long double x = 0, y = 0;
+    for(unsigned int i=0; i < PRECISION*2; ++i)
     {
         double nX, nY;
         nX = (x * x) - (y * y) + aX;
@@ -67,21 +103,22 @@ int borned(double aX, double aY)
     return 0;
 }
 
-void drawMandelbrot(double xMin, double xMax, double yMin, double yMax, double coefX, double coefY, SDL_Renderer * renderer)
+void drawMandelbrot(long double xMin, long double xMax, long double yMin, long double yMax, long double coefX, long double coefY, SDL_Renderer * renderer)
 {
     for(int i=0; i<WIDTH; ++i)
         for(int j=0; j<HEIGHT; ++j)
         {
-            double x = (i / coefX) + xMin;
-            double y = (j / coefY) + yMin;
+            long double x = (i / coefX) + xMin;
+            long double y = (j / coefY) + yMin;
             int pow = borned(x, y);
             int color = (2*255*pow) / PRECISION;
-            if(color > 255)
+            if(color >= 255)
                 color = 255;
             if(pow > 0)
+                //HSVcolor(color, renderer);
                 SDL_SetRenderDrawColor(renderer, 0, color, 0, 255);
             else
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderDrawPoint(renderer, i, j);
         }
 
@@ -102,23 +139,24 @@ int main(int argc, char** argv)
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    double goalX = -0.743643887037151;
-    double goalY =  0.13182590420533;
+    long double goalX = -0.743643887037151;
+    long double goalY =  0.13182590420533;
     double zoom = (double)1/10;
 
     double ratio = (double)WIDTH/HEIGHT;
 
-    double xMin = goalX - 1.5*ratio, xMax = goalX + 1.5*ratio, yMin = goalY - 1.5, yMax = goalY + 1.5;
+    long double xMin = goalX - 1.5*ratio, xMax = goalX + 1.5*ratio, yMin = goalY - 1.5, yMax = goalY + 1.5;
     //double xMin = -2, xMax = 1, yMin = -1.5, yMax = 1.5;
 
-    double coefX = double(WIDTH) / (xMax - xMin);
-    double coefY = double(HEIGHT) / (yMax - yMin);
+    long double coefX = double(WIDTH) / (xMax - xMin);
+    long double coefY = double(HEIGHT) / (yMax - yMin);
 
-    for(int i=0; i<300; ++i)
+    for(int i=0; i<400; ++i)
     {
+        cout << '\r' << i << endl;
         if(true || i > 190)
             drawMandelbrot(xMin, xMax, yMin, yMax, coefX, coefY, renderer);
-        saveScreenshotBMP("./images/big/", i, window, renderer);
+        saveScreenshotBMP("./images/black/", i, window, renderer);
         xMin += (goalX - xMin) * zoom;
         xMax += (goalX - xMax) * zoom;
         yMin += (goalY - yMin) * zoom;
